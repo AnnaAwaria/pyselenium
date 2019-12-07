983e45975fe18e6ee5a5eb8e1ed9992aa811b507
AnnaAwaria <retrorain@gmail.com>
2019-12-07 10:25:31 +0100

    Absolut Zero task

---
+++ zeroabsolutne.py
@@ -0,0 +1,17 @@
+from liczba import Number
+
+
+class AbsolutZero(Number):
+    def __init__(self):
+        self.value = 0
+
+    def __str__(self):
+        return 'Jestem zerem!'
+
+    def reklama(self):
+        print('Ucz się!')
+
+
+zero = AbsolutZero()
+print(zero)
+print(AbsolutZero().reklama())
